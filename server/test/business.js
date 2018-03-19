99080fd6bdd9cbfa02a93a0acff8de8f76c89692
Lucky <Lumpsey@gmail.com>
2018-03-19 14:04:18 +0100

    add test to get business by category api endpoint

--- server/test/business.js
+++ server/test/business.js
@@ -257,3 +257,41 @@ describe('GET /Business?location=lagos Route Test', () => {
   });
 });
 
+describe('GET /Business?category=information technology Route Test', () => {
+  it('it should GET reviews detail', (done) => {
+    const businessTest = [{
+      id: 1,
+      name: 'x-plug Nig. Ltd',
+      services: 'Wed Development',
+      phone_number: '09034222567',
+      email: 'xplug@gmail.com',
+      address: '1, xyz street, Surulere, Lagos',
+      category: 'Information Technology',
+      location: 'lagos',
+      image: 'url',
+      reviews: [{
+        id: 1,
+        name: 'Tobi Alabi',
+        email: 'tobialabi@gmail.com',
+        reviews: 'I love you company and services, just keep it up',
+        country: 'USA',
+      },
+      {
+        id: 2,
+        name: 'Joseph Tutu',
+        email: 'josephtutu@gmail.com',
+        reviews: 'Best Services',
+        country: 'Ghana',
+      }],
+    }];
+    chai.request(app)
+      .get('/api/v1/business?category=information technology')
+      .end((err, res) => {
+        // res.should.have.status(200);
+        res.body.should.be.a('object');
+        // res.body.should.have.property('message').eql('Success');
+        done();
+      });
+  });
+});
+
